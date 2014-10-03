a4aa8e692963106e3a88b79e7d9293a19129f8e1
Israel Ochoa <iochoa15@hotmail.com>
2014-10-02 21:10:35 -0400

    Almost done

---
+++ GA_PROJECT_03_PHOTO_GALLERY/js/application.js
@@ -0,0 +1,26 @@
+var images = ["img/img1.png", "img/img2.png", "img/img3.png", "img/img4.png"]
+var currentImage = 0
+
+
+function changePicture(whichNum){
+	var img = document.getElementById('image');
+	img.setAttribute('src', images[whichNum]);
+	currentImage = whichNum;
+}
+function onClickNext() {
+	currentImage++;
+	if(currentImage === images.length){
+		currentImage = 0;
+	}
+	var img = document.getElementById('image');
+	img.setAttribute('src', images[currentImage]);
+	
+}
+function onClickPrev() {
+	currentImage = currentImage -1;
+	if(currentImage === -1){
+		currentImage = images.length -1;
+	}
+	var img = document.getElementById('image');
+	img.setAttribute('src', images[currentImage]);	
+}
